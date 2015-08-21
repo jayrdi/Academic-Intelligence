7a6e309813cc1c88a4b137377acb1458dcf143da
John Dawson <john.dawson@ncl.ac.uk>
2015-08-21 11:53:50 +0100

    Edit
    Removed redundant files

--- academic_intelligence/public/retired/script.js
+++
@@ -1,7 +0,0 @@
-// this will unhide extra search fields when user clicks relevant button
-function unhide(divID) {
-	var item = document.getElementById(divID);
-	if (item) {
-		item.className = (item.className == 'hidden')?'unhidden well bs-component':'hidden';
-	};
-};
